1fbc504c818c772dfb6bc2a4f2ae8a2d9f31aad4
marndl <software@marndl.dev>
2025-12-10 23:40:29 +0100

    feat: add template for empty day module

---
+++ src/days/dayXX.rs
@@ -0,0 +1,22 @@
+pub fn part1(input: &str) -> String {
+    "Solve part1".to_owned()
+}
+
+pub fn part2(input: &str) -> String {
+    "Solve part2".to_owned()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn dayXX_part1() {
+        assert_eq!(part1(""), "Solve part1");
+    }
+
+    #[test]
+    fn dayXX_part2() {
+        assert_eq!(part2(""), "Solve part2");
+    }
+}
